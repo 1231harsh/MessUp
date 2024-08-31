ef7358dc913e4799ff307be9398315f43f07f665
1231harsh <134150836+1231harsh@users.noreply.github.com>
2024-08-31 13:10:01 +0530

    Update WebSocketConfig.java
    Added cross origin to websocket

--- messageApplication/src/main/java/com/hc/messageApplication/webSocket/WebSocketConfig.java
+++ messageApplication/src/main/java/com/hc/messageApplication/webSocket/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.hc.messageApplication.webSocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,6 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry ) {
-		registry.addEndpoint("/chat").withSockJS();
+		registry.addEndpoint("/chat")
+			.setAllowedOrigins("https://messup-production.up.railway.app")
+			.withSockJS();
 	}
 }
