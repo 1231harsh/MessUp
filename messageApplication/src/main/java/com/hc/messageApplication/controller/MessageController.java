8694cdb5660b1959b4ac6100a407a6decf7436e9
1231harsh <134150836+1231harsh@users.noreply.github.com>
2024-08-31 13:02:42 +0530

    Update MessageController.java

--- messageApplication/src/main/java/com/hc/messageApplication/controller/MessageController.java
+++ messageApplication/src/main/java/com/hc/messageApplication/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.hc.messageApplication.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
